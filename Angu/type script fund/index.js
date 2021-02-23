fa7fb68210535fd3592c13c965a4718f6f01ed7f
Fatma <fatmaammdou7@gmail.com>
2021-02-23 20:55:18 +0200

    init

---
+++ Angu/type script fund/index.js
@@ -0,0 +1,13 @@
+// function anything(){
+//     for(var i = 0; i<5;i++){
+//         console.log(i);
+//     }
+//     console.log("Finally "+i);
+// }
+
+// anything();
+
+var count =5;
+count ='a';
+
+
